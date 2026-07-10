fa2d00af79d6037b282426dfbba555b176bedc63
Jean-Baptiste DALLE <jean-baptiste.dalle@stereograph.fr>
2026-07-10 08:50:39 +0200

    feat(deploy): implement pipeline to deploy on github page

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/roldaices-character-sheets/',
   plugins: [react(), tailwindcss()],
   test: {
     environment: 'jsdom',
